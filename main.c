ef0a0f80b60249842bfe03b3cb27e52995fe3817
Lovro Oreskovic <lovro@oreskovic.me>
2018-08-02 23:07:26 +0200

    add debug symbols and some code to test breakpoints

--- main.c
+++ main.c
@@ -5,4 +5,11 @@
 int main( int argc, const char* argv[] )
 {
 	printf( "\nHello World\n\n" );
+
+    for (int i = 0; i < 10; i++){
+      printf("test msg variable i currently has a value of %d\n", i);
+    }
+    printf("\n");
+    
+    return 0;
 }
